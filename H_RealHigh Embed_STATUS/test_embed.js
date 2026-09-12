51f598b87656efb6f9d61d7a43fdf3704644a58e
THANAKRIT2010 <qq.thanakrit06@gmail.com>
2026-09-12 10:51:11 +0700

    ลบ

--- H_RealHigh Embed_STATUS/test_embed.js
+++
@@ -1,7 +0,0 @@
-const { Embed_Start } = require('./Embed_Start.js');
-try {
-    const embed = Embed_Start();
-    console.log(embed);
-} catch (e) {
-    console.error("Test failed:", e);
-}
--- H_RealHigh UpDate_Setting/H_RealHigh utils/Time.js
+++
@@ -1,6 +0,0 @@
-function getThaiTime() {
-    const thai = new Date().toLocaleString('en-GB', { timeZone: 'Asia/Bangkok' });
-    const [date, time] = thai.split(', ');
-    return { date: date.replace(/\//g, '-'), time, full: `${time} ${date.replace(/\//g, '-')}` };
-}
-module.exports = { getThaiTime };
